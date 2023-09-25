6109a88ae72d1dc9d88a1ada056d3e5bc120fc05
Tarun Menon <tarunmenon95@gmail.com>
2023-09-25 16:44:26 +1000

    Bump python to 3.8 due to 3.7 EOL

--- s3-cleanup-on-delete.cfndsl.rb
+++ s3-cleanup-on-delete.cfndsl.rb
@@ -107,7 +107,7 @@ CloudFormation do
       CODE
     })
     Handler "index.lambda_handler"
-    Runtime "python3.7"
+    Runtime "python3.8"
     Role FnGetAtt(:CleanBucketOnDeleteRole, :Arn)
     Timeout 60
   }
